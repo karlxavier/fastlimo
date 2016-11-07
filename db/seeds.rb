b3732b49fe2743ffe971702291cee548e9609bd8
Karl Xavier <kxdc1025@yahoo.com>
2016-11-07 14:03:58 -0500

    change user sign up

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(fullname: 'Karl Xavier del Campo', email: 'karlxavier@enguae.com',super: true, password: 'rupert10')
